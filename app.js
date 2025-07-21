c197079db41f2b417f19bd186c49f21ce710cd23
kmroja <rojakmr25@gmail.com>
2025-07-21 14:08:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const protectedRoute = require("./routes/protectedRoute");
+
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+const app = express();
+
+
+app.use(cors());
+app.use("/api/auth", authRoutes); 
+app.use(express.json()); 
+app.use("/api/protected", protectedRoute);  
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(" MongoDB Connected");
+    app.listen(5000, () => {
+      console.log(" Server running on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.error(" Failed to connect to MongoDB:", err.message);
+  });
---
+++ controllers/authController.js
@@ -0,0 +1,44 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const existing = await User.findOne({ email });
+    if (existing) return res.status(400).json({ msg: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ username, email, password: hashedPassword });
+
+    res.status(201).json({ msg: 'User registered successfully' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
