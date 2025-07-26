8ed6a056497ea26cc2697662139ef84785affbea
kmroja <rojakmr25@gmail.com>
2025-07-26 12:39:03 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,8 +26,9 @@ exports.login = async (req, res) => {
     const match = await bcrypt.compare(password, user.password);
     if (!match) return res.status(400).json({ msg: 'Invalid credentials' });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
     res.json({ token });
+    
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
